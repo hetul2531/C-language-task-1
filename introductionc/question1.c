a287656dd7d070d3abcb293dcf5501e90988ec2b
hetul2531 <hetulpatel9479@gmail.com>
2024-02-07 17:46:14 +0530

    kai bi lakhi sakay

---
+++ introductionc/question1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void){
+  char name [] = "Hetul patel";
+  int age = 23;
+  char school [] = "AB school";
+
+  printf("Name %s\n", name );
+  printf("Age %d\n" , age);
+  printf("School %s\n", school );
+  
+ 
+ return 0;
+
+}
---
+++ introductionc/question2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void){
+
+    int HRA;
+    int DA;
+    int BS;
+    int TA;
+    
+ printf(" Enter value of HRA");
+ scanf("%d" , &HRA);
+ printf(" Enter value of DA");
+ scanf("%d" , &DA );
+ printf(" Enter value of BS");
+ scanf("%d" , &BS );
+ printf(" Enter value of TA");
+ scanf("%d" , &TA);
+
+ printf("ans %d" , HRA + DA + BS + TA);
+
+ return 0;
+
+}
